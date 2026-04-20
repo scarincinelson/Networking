a155f5776eba9745734664275ab4676535256409
Nelson Scarinci <scarincinelson@gmail.com>
2026-04-20 19:36:35 -0300

    Fix input other player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,39 +7,35 @@ public class PlayerController : NetworkBehaviour
     CharacterController _characterController;
     Vector2 _input;
     float _speed = 5f;
+    private PlayerInput playerInput;
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.enabled = false;
     }
 
     private void Update()
     {
-        //if (!IsOwner) return;
-
-        if (Keyboard.current.dKey.wasPressedThisFrame)
-        {
-            this.transform.position = new Vector3(transform.position.x + 1, 0, 0);
-        }
-
-        if (Keyboard.current.aKey.wasPressedThisFrame)
-        {
-            this.transform.position = new Vector3(transform.position.x - 1, 0, 0);
-        }
-
-        /*
         Vector3 move = new Vector3(_input.x, 0, _input.y);
-        Debug.Log(move);
-
         _characterController.Move(move * _speed * Time.deltaTime);
-        */
     }
 
-    /*
+    public override void OnNetworkSpawn()
+    {
+        playerInput.enabled = IsOwner;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        playerInput.enabled = false;
+    }
+
 
     public void OnMove(InputValue value)
     {
         _input = value.Get<Vector2>();
     }
-    */
+
 }
