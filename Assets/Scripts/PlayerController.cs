e0ce6c84f0d2db73d9236dbed33a1144a821a567
Nelson Scarinci <scarincinelson@gmail.com>
2026-04-20 20:55:41 -0300

    Added camera + improved movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,21 +18,63 @@ public class PlayerController : NetworkBehaviour
         _playerInput.enabled = false;
     }
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     private void Update()
     {
         if (!IsOwner) return;
 
-        Vector3 move = new Vector3(_input.x, 0, _input.y);
+        Vector3 moveDirection = GetCameraRelativeDirection();
 
+        RotateCharacter(moveDirection);
+        moveDirection = ApplyGravity(moveDirection);
+        MoveCharacter(moveDirection);
+    }
+
+    private Vector3 GetCameraRelativeDirection()
+    {
+        Transform cam = Camera.main.transform;
+
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward.Normalize();
+        camRight.Normalize();
+
+        return camRight * _input.x + camForward * _input.y;
+    }
+
+    private Vector3 ApplyGravity(Vector3 moveDirection)
+    {
         if (_characterController.isGrounded && _yVelocity < 0)
         {
             _yVelocity = -2f;
         }
 
         _yVelocity += _gravity * Time.deltaTime;
-        move.y = _yVelocity;
+        moveDirection.y = _yVelocity;
 
-        _characterController.Move(move * _speed * Time.deltaTime);
+        return moveDirection;
+    }
+    private void RotateCharacter(Vector3 moveDirection)
+    {
+        if (moveDirection.magnitude <= 0.1f) return;
+
+        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,10f * Time.deltaTime);
+    }
+
+    private void MoveCharacter(Vector3 moveDirection)
+    {
+        _characterController.Move(moveDirection * _speed * Time.deltaTime);
     }
 
     public override void OnNetworkSpawn()
@@ -45,10 +87,8 @@ public class PlayerController : NetworkBehaviour
         _playerInput.enabled = false;
     }
 
-
     public void OnMove(InputValue value)
     {
         _input = value.Get<Vector2>();
     }
-
 }
