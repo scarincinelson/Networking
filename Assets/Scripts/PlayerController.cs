077c032ab07938d78357aaf301f5371925edb51e
Nelson Scarinci <scarincinelson@gmail.com>
2026-04-20 20:18:38 -0300

    Added gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,32 +4,45 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : NetworkBehaviour
 {
-    CharacterController _characterController;
-    Vector2 _input;
-    float _speed = 5f;
-    private PlayerInput playerInput;
+    private CharacterController _characterController;
+    private Vector2 _input;
+    private float _speed = 5f;
+    private PlayerInput _playerInput;
+    private float _yVelocity;
+    private float _gravity = -9.81f;
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
-        playerInput = GetComponent<PlayerInput>();
-        playerInput.enabled = false;
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.enabled = false;
     }
 
     private void Update()
     {
+        if (!IsOwner) return;
+
         Vector3 move = new Vector3(_input.x, 0, _input.y);
+
+        if (_characterController.isGrounded && _yVelocity < 0)
+        {
+            _yVelocity = -2f;
+        }
+
+        _yVelocity += _gravity * Time.deltaTime;
+        move.y = _yVelocity;
+
         _characterController.Move(move * _speed * Time.deltaTime);
     }
 
     public override void OnNetworkSpawn()
     {
-        playerInput.enabled = IsOwner;
+        _playerInput.enabled = IsOwner;
     }
 
     public override void OnNetworkDespawn()
     {
-        playerInput.enabled = false;
+        _playerInput.enabled = false;
     }
 
 
